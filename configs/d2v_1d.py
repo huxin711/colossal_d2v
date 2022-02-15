1120927a11f81bfae743264fc9e644a5b83ca0d9
huxin711 <dr.huxin711@foxmail.com>
2022-02-15 11:10:45 +0800

    config files

---
+++ configs/d2v_1d.py
@@ -0,0 +1,22 @@
+BATCH_SIZE = 128
+LEARNING_RATE = 2e-3
+WEIGHT_DECAY = 3e-2
+
+TENSOR_PARALLEL_SIZE = 2
+TENSOR_PARALLEL_MODE = '1d'
+
+NUM_EPOCHS = 800
+WARMUP_EPOCHS = 40
+max_norm = 2.
+
+parallel = dict(
+    pipeline=1,
+    tensor=dict(mode=TENSOR_PARALLEL_MODE, size=TENSOR_PARALLEL_SIZE),
+)
+
+seed = 77
+
+LOG_PATH = f"./vit_{TENSOR_PARALLEL_MODE}_cifar10_tp{TENSOR_PARALLEL_SIZE}_bs{BATCH_SIZE}_lr{LEARNING_RATE}/"
+
+
+find_unused_parameters = True
